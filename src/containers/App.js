b0007c66c5d4b5d9a4e5fb58dd1e8033fc20d17c
billalp <billal.patel@rightmove.co.uk>
2020-01-18 22:55:09 +0000

    Updated everything

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,37 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment, useState } from 'react'
 import axios from 'axios'
-import './App.css'
 
 import PageHeader from '../components/PageHeader'
 import DividerLine from '../components/DividerLine'
 import JokeButton from '../components/JokeButton'
 import JokeMessage from '../components/JokeMessage'
+import LoadingSpinner from '../components/LoadingSpinner'
+import './App.css'
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      loading: false,
-      resultsData: []
-    }
-  }
 
-  componentDidMount() {
-    
-  }
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  const [resultsData, setResultsData] = useState([]);
 
-  reset() {
-      this.setState({
-          resultsData: []
-      })
-  }
-
-  onJokeButtonClick = () => {
+  const onJokeButtonClick = () => {
     axios.get('https://sv443.net/jokeapi/category/programming')
       .then(res => {
         const result = res.data;
-        this.setState({
-          loading: true,
-          resultsData: result
-        })
+          setLoading(true);
+          setResultsData(result);
+          setLoading(true);
       })
   }
 
-  render() {
-    const { resultsData, loading } = this.state
-
     return (
-      <Fragment>
+      <>
         <PageHeader />
         <DividerLine />
-        <JokeButton getJoke={ this.onJokeButtonClick } />
-        <JokeMessage jokeline={ resultsData } loader={ loading } />
-      </Fragment>
+        <JokeButton getJoke={ onJokeButtonClick } />
+        <LoadingSpinner />
+        <JokeMessage jokeline={ resultsData } />
+      </>
     )
-  }
 }
 
 export default App
